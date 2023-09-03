cd4b0c0ca3a351a3a1aad28594d4beea92e3a041
prajwal raut <90103440+Prajwalraut29@users.noreply.github.com>
2023-09-03 14:17:24 +0530

    build chat bot

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = "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";
+const API_KEY = "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";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
